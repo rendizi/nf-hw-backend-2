c7ac3fac277327778e25bc3ba0c437212250ce1a
rendizi <rendizi_off@gmail.com>
2024-06-13 09:03:12 +0500

    update

--- src/auth/auth-router.ts
+++ src/auth/auth-router.ts
@@ -2,11 +2,14 @@ import { Router } from 'express';
 import { authMiddleware } from '../middlewares/auth-middleware';
 import AuthController from './auth-controller';
 import AuthService from './auth-service';
+import cors from "cors";
 
 const authRouter = Router();
 
 const authService = new AuthService();
 const authController = new AuthController(authService);
+
+authRouter.use(cors())
 
 authRouter.post('/register', authController.registerUser);
 authRouter.post('/login', authController.loginUser);
--- src/global-router.ts
+++ src/global-router.ts
@@ -2,10 +2,11 @@ import { Router } from 'express';
 import authRouter from './auth/auth-router';
 // other routers can be imported here
 import roomsRouter from './rooms/rooms-router';
+import cors from "cors";
 
 const globalRouter = Router();
 
-
+globalRouter.use(cors())
 globalRouter.use(authRouter);
 globalRouter.use(roomsRouter)
 
